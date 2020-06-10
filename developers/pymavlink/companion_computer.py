3464d180a092c59b5537da0072f99e248639814e
Willian Galvani <williangalvani@gmail.com>
2020-06-10 18:40:36 -0300

    companion_computer.py: fix time argument so it is an integer
    otherwise pymavlink was unable to pack the message

--- developers/pymavlink/companion_computer.py
+++ developers/pymavlink/companion_computer.py
@@ -7,7 +7,7 @@ def wait_conn(master):
     msg = None
     while not msg:
         master.mav.ping_send(
-            time.time(), # Unix time
+            int(time.time()), # Unix time
             0, # Ping number
             0, # Request ping of all systems
             0 # Request ping of all components
