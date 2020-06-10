dbb99004082d85daf560bf0a189a0171a9828477
Willian Galvani <williangalvani@gmail.com>
2020-06-10 18:54:29 -0300

    rc_joystick.py: make pylint happy

--- developers/pymavlink/rc_joystick.py
+++ developers/pymavlink/rc_joystick.py
@@ -1,3 +1,9 @@
+"""
+Example of how to use RC_CHANNEL_OVERRIDE messages to force input channels
+in Ardupilot. These effectively replace the input channels (from joystick
+or radio), NOT the output channels going to thrusters and servos.
+"""
+
 # Import mavutil
 from pymavlink import mavutil
 
@@ -9,20 +15,20 @@ master.wait_heartbeat()
 # Create a function to send RC values
 # More information about Joystick channels
 # here: https://www.ardusub.com/operators-manual/rc-input-and-output.html#rc-inputs
-def set_rc_channel_pwm(id, pwm=1500):
+def set_rc_channel_pwm(channel_id, pwm=1500):
     """ Set RC channel pwm value
     Args:
-        id (TYPE): Channel ID
+        channel_id (TYPE): Channel ID
         pwm (int, optional): Channel pwm value 1100-1900
     """
-    if id < 1 or id > 18:
+    if channel_id < 1 or channel_id > 18:
         print("Channel does not exist.")
         return
 
     # Mavlink 2 supports up to 18 channels:
     # https://mavlink.io/en/messages/common.html#RC_CHANNELS_OVERRIDE
     rc_channel_values = [65535 for _ in range(18)]
-    rc_channel_values[id - 1] = pwm
+    rc_channel_values[channel_id - 1] = pwm
     master.mav.rc_channels_override_send(
         master.target_system,                # target_system
         master.target_component,             # target_component
