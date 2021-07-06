d31b8b6a201204ee9404bb2ff8c7b559ed35d3b6
ES-Alexander <sandman.esalexander@gmail.com>
2021-07-06 11:29:40 +1000

    pymavlink: check valid connection (wait_heartbeat)

--- developers/pymavlink/udp_connection.py
+++ developers/pymavlink/udp_connection.py
@@ -21,6 +21,9 @@ from pymavlink import mavutil
 #  E.g: --out udpbcast:192.168.2.255:yourport
 master = mavutil.mavlink_connection('udpin:0.0.0.0:14550')
 
+# Make sure the connection is valid
+master.wait_heartbeat()
+
 # Get some information !
 while True:
     try:
