2ce37b5f5486f6ee42660e5b65b5b9d0bc42d7cc
ES-Alexander <sandman.esalexander@gmail.com>
2021-07-06 11:23:04 +1000

    pymavlink: include arming checks and confirmation-waits

--- developers/pymavlink/arm_disarm.py
+++ developers/pymavlink/arm_disarm.py
@@ -20,6 +20,11 @@ master.mav.command_long_send(
     0,
     1, 0, 0, 0, 0, 0, 0)
 
+# wait until arming confirmed (can manually check with master.motors_armed())
+print("Waiting for the vehicle to arm")
+master.motors_armed_wait()
+print('Armed!')
+
 # Disarm
 # master.arducopter_disarm() or:
 master.mav.command_long_send(
@@ -28,3 +33,6 @@ master.mav.command_long_send(
     mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
     0,
     0, 0, 0, 0, 0, 0, 0)
+
+# wait until disarming confirmed
+master.motors_disarmed_wait()
